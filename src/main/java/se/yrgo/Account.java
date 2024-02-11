e938ba7797da2f5cd0e675c659db068644d8955f
TomasYrgo <150817213+TomasYrgo@users.noreply.github.com>
2024-02-11 11:46:17 +0100

    Implementing Account variables

--- src/main/java/se/yrgo/Account.java
+++ src/main/java/se/yrgo/Account.java
@@ -1,4 +1,40 @@
 package se.yrgo;
 
+
+import java.util.*;
+
 public class Account {
+    private String accountNumber;
+    private double balance;
+    private List<Transaction> transactions;
+
+    public Account(java.lang.String accountNumber, double balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void deposit(double amount){
+
+    }
+
+    public void withdraw(double amount){
+
+    }
+
+    public void transfer(Account receiverAccount, double amount){
+
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Transaction> getTransactions() {
+        return transactions;
+    }
 }
