bb5ed54000f0b3f05d87740edac1831bb8f08e43
helben2002 <helben2002@skola.goteborg.se>
2024-02-12 16:13:05 +0100

    Adding test to AccountTest

--- src/test/java/se/yrgo/AccountTest.java
+++ src/test/java/se/yrgo/AccountTest.java
@@ -1,4 +1,29 @@
 package se.yrgo;
 
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class AccountTest {
+
+    Account account = new Account("123456", 0);
+
+
+    @Test
+    public void testDepositPositiveAmount(){
+        double startBalance = account.getBalance();
+        double deposit = 100;
+        account.deposit(deposit);
+        assertEquals(startBalance + deposit, account.getBalance());
+    }
+
+    // denna failar just nu eftersom allt inte är klart i klasserna, men borde funka sen ->
+    @Test
+    public void testAddingTransaction(){
+        double deposit = 100;
+        account.deposit(deposit);
+        assertEquals(1, account.getTransactions().size());
+    }
+
 }
