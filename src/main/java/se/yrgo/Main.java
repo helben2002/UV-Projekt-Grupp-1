3274f91feb849c6490a21475e92345e6be62b457
helben2002 <helben2002@skola.goteborg.se>
2024-02-13 15:22:58 +0100

    adding loop and log out on menu in main

--- src/main/java/se/yrgo/Main.java
+++ src/main/java/se/yrgo/Main.java
@@ -10,39 +10,44 @@ public class Main {
         Account account1 = new Account("454545");
         Customer randomGuy = new Customer("Kim");
 
-        String choice;
-        double value;
+        while (true) {
+            String choice;
+            double value;
 
-        System.out.println("Help on:");
-        System.out.println(" 1. Check balance");
-        System.out.println(" 2. Make deposit");
-        System.out.println(" 3. Make withdraw");
-        System.out.println(" 4. Check transfer history");
-        System.out.print("Choose one: ");
+            System.out.println("Help on:");
+            System.out.println(" 1. Check balance");
+            System.out.println(" 2. Make deposit");
+            System.out.println(" 3. Make withdraw");
+            System.out.println(" 4. Check transfer history");
+            System.out.println(" 5. Log out");
+            System.out.print("Choose one: ");
 
-        choice = scan.next();
+            choice = scan.next();
 
-        System.out.println("\n");
+            System.out.println("\n");
 
-        switch(choice) {
-            case "1":
-                System.out.println(account1.getBalance());
-                break;
-            case "2":
-                System.out.println("Value to deposit: ");
-                value = scan.nextDouble();
-                account1.deposit(value);
-                break;
-            case "3":
-                System.out.println("Value to withdraw: ");
-                value = scan.nextDouble();
-                account1.withdraw(value);
-                break;
-            case "4":
-                System.out.println(account1.getTransactions());
-                break;
-            default:
-                System.out.println("Selection not found.");
+            switch (choice) {
+                case "1":
+                    System.out.println(account1.getBalance());
+                    break;
+                case "2":
+                    System.out.println("Value to deposit: ");
+                    value = scan.nextDouble();
+                    account1.deposit(value);
+                    break;
+                case "3":
+                    System.out.println("Value to withdraw: ");
+                    value = scan.nextDouble();
+                    account1.withdraw(value);
+                    break;
+                case "4":
+                    System.out.println(account1.getTransactions());
+                    break;
+                case "5":
+                    return;
+                default:
+                    System.out.println("Selection not found.");
+            }
         }
     }
 }
