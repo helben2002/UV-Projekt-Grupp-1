0b457129cb7fcfcbc942766b730b8436fab6221b
Madasa-cell <adampaajarvi@gmail.com>
2024-02-13 13:29:23 +0100

    Adding Customer.java

--- src/main/java/se/yrgo/Customer.java
+++ src/main/java/se/yrgo/Customer.java
@@ -1,4 +1,27 @@
 package se.yrgo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
+    //Create atleast 2 Customers through Account
+    private String name;
+    private List<Account> accounts;
+
+    public Customer(String name){
+        this.name = name;
+        this.accounts = new ArrayList<>();
+    }
+
+    public void addAccount(Account account){
+        accounts.add(account);
+    }
+
+    public List<Account> geAccounts(){
+        return accounts;
+    }
+
+    public void transfer(Account recipient, double amount, String description){
+        // Transfer funds from this account to the recipient's account
+    }
 }
