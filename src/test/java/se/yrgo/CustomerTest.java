a87d73c014f3217efa8c29d195ef6d97c469f4c9
Madasa-cell <adampaajarvi@gmail.com>
2024-02-15 11:36:25 +0100

    Added JUnit Tests for all of those deprecated methods

--- src/test/java/se/yrgo/CustomerTest.java
+++ src/test/java/se/yrgo/CustomerTest.java
@@ -3,6 +3,7 @@ package se.yrgo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class CustomerTest {
@@ -36,5 +37,62 @@ public class CustomerTest {
         //Verifying that the Customer has a Account.
         assertEquals(1, customer.getAccounts().size());
         assertTrue(customer.getAccounts().contains(account));
+    }
+
+    @Test
+    void testGetCurrentAccount(){
+        Customer customer = new Customer("Alex Gellato");
+        Account account1 = new Account("123");
+        customer.addAccount(account1);
+
+        customer.selectAccount("123");
+        assertEquals(account1, customer.getCurrentAccount());
+    }
+
+    @Test
+    void testSelectAccount(){
+        Customer customer = new Customer("John Stew");
+        Account account1 = new Account("123");
+        Account account2 = new Account("456");
+
+        customer.addAccount(account1);
+        customer.addAccount(account2);
+
+        // Selecting account1
+        customer.selectAccount("123");
+
+        // Verifying selected Account
+        assertEquals(account1, customer.getCurrentAccount());
+
+        // Switching to account2
+        customer.selectAccount("456");
+
+        // Verifying that the Account has been switched
+        assertEquals(account2, customer.getCurrentAccount());
+    }
+
+    @Test
+    void testTransferFunds(){
+        Customer customer = new Customer("John Stew");
+        Account account1 = new Account("123");
+        Account account2 = new Account("456");
+
+        customer.addAccount(account1);
+        customer.addAccount(account2);
+
+        // Depositing funds into account1
+        account1.deposit(1000.0);
+
+        // Selecting account1
+        customer.selectAccount("123");
+
+        // Transferring funds from account1 to account2
+        double amountToTransfer = 500.0;
+        customer.transferFunds(account2, amountToTransfer);
+
+        // Verifying transfer
+        Assertions.assertEquals(500.0, account1.getBalance(), 0.01);
+        assertEquals(500.0, account2.getBalance(), 0.01);
+        //The delta value of '0.01' for the 'assertEquals' calls to account for small differences in double precision.
     }
 }
